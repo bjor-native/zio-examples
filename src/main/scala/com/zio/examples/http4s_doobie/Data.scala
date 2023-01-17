d0efc803a410a0b49bcbd2be98c1f74b8fbcbaff
tk7306 <akhorkov@teko.io>
2023-01-17 12:03:23 +0300

    --

--- src/main/scala/com/zio/examples/http4s_doobie/Data.scala
+++ src/main/scala/com/zio/examples/http4s_doobie/Data.scala
@@ -3,5 +3,3 @@ package com.zio.examples.http4s_doobie
 final case class User(id: Int, name: String)
 
 final case class UserNotFound(id: Int) extends Exception
-
-final case class TestCaseClass(id: String)
